993abc8dfebbcd8d6756c215866f594c4b4cdc67
봄 <167315197+bom0320@users.noreply.github.com>
2025-06-16 17:41:31 +0900

    Initial commit

---
+++ setup/fsd/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Toaster } from 'sonner';
+import { QueryProvider } from '@/shared/lib/query';
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className={`${inter.variable} antialiased`}>
+        <QueryProvider>
+          {children}
+          <Toaster position="top-center" expand={true} richColors />
+        </QueryProvider>
+      </body>
+    </html>
+  );
+}
---
+++ setup/fsd/src/app/signin/page.tsx
@@ -0,0 +1,9 @@
+const Signin = () => {
+  return (
+    <div>
+      <h1>SignInPage</h1>
+    </div>
+  );
+};
+
+export default Signin;
